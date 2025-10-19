022100dbf54524c0670e686152377466ea436dd3
luluisfcm <127862285+luluisfcm@users.noreply.github.com>
2025-10-19 20:23:10 +0100

    hap Appointment

--- hap-appointmentrecords/src/main/java/leti_sisdis_6/hapappointmentrecords/HapAppointmentrecordsApplication.java
+++ hap-appointmentrecords/src/main/java/leti_sisdis_6/hapappointmentrecords/HapAppointmentrecordsApplication.java
@@ -6,28 +6,18 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = {
-        org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration.class,
-        org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration.class,
-        org.springframework.boot.autoconfigure.mongo.MongoReactiveAutoConfiguration.class
-})
-@ComponentScan(basePackages = {
-        "leti_sisdis_6.hapappointmentrecords",
-        "leti_sisdis_6.hapauth.api",
-        "leti_sisdis_6.hapauth.usermanagement",
-        "leti_sisdis_6.hapauth.services",
-        "leti_sisdis_6.hapauth.configuration"
+@SpringBootApplication(
+        scanBasePackages = {
+                "leti_sisdis_6.hapappointmentrecords",
+        }
+)
+@EnableJpaRepositories(basePackages = {
+        "leti_sisdis_6.hapappointmentrecords.repository",
+        "leti_sisdis_6.happhysicians.repository"
 })
 @EntityScan(basePackages = {
         "leti_sisdis_6.hapappointmentrecords.model",
-        "leti_sisdis_6.happhysicians.model",
-        "leti_sisdis_6.hapauth.model",
-        "leti_sisdis_6.hapauth.usermanagement"
-})
-@EnableJpaRepositories(basePackages = {
-        "leti_sisdis_6.hapappointmentrecords.repository",
-        "leti_sisdis_6.happhysicians.repository",
-        "leti_sisdis_6.hapauth.usermanagement"
+        "leti_sisdis_6.happhysicians.model"
 })
 public class HapAppointmentrecordsApplication {
     public static void main(String[] args) {
