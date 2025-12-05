8fe1dc93c58271eb866e71159fb57d5e9e8b3c09
luluisfcm <127862285+luluisfcm@users.noreply.github.com>
2025-12-05 00:13:59 +0000

    config mongo rabbit docker

--- hap-appointmentrecords/src/main/java/leti_sisdis_6/hapappointmentrecords/HapAppointmentrecordsApplication.java
+++ hap-appointmentrecords/src/main/java/leti_sisdis_6/hapappointmentrecords/HapAppointmentrecordsApplication.java
@@ -2,8 +2,12 @@ package leti_sisdis_6.hapappointmentrecords;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("leti_sisdis_6.hapappointmentrecords.repository")
+@EnableMongoRepositories("leti_sisdis_6.hapappointmentrecords.mongo")
 public class HapAppointmentrecordsApplication {
     public static void main(String[] args) {
         SpringApplication.run(HapAppointmentrecordsApplication.class, args);
---
+++ hap-appointmentrecords/src/main/java/leti_sisdis_6/hapappointmentrecords/config/RabbitMQConfig.java
@@ -0,0 +1,53 @@
+package leti_sisdis_6.hapappointmentrecords.config;
+
+import org.slf4j.MDC;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String CORRELATION_ID_HEADER = "X-Correlation-Id";
+    public static final String DEFAULT_EXCHANGE = "hap-appointmentrecords-exchange";
+
+    @Value("${hap.rabbitmq.exchange:" + DEFAULT_EXCHANGE + "}")
+    private String exchangeName;
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(exchangeName);
+    }
+
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+        RabbitAdmin admin = new RabbitAdmin(connectionFactory);
+        admin.setAutoStartup(true);
+        admin.declareExchange(exchange());
+        return admin;
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter producerMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(producerMessageConverter());
+        rabbitTemplate.setBeforePublishPostProcessors(message -> {
+            String correlationId = MDC.get(CORRELATION_ID_HEADER);
+            if (correlationId != null && !correlationId.isBlank()) {
+                message.getMessageProperties().setHeader(CORRELATION_ID_HEADER, correlationId);
+            }
+            return message;
+        });
+        return rabbitTemplate;
+    }
+}
