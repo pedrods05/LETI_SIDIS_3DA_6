50a496733ee9c119bfa153f725d62f1bef219dc7
luluisfcm <127862285+luluisfcm@users.noreply.github.com>
2025-12-06 17:29:24 +0000

    updated

---
+++ hap-appointmentrecords/src/main/java/leti_sisdis_6/hapappointmentrecords/service/event/AppointmentCanceledEvent.java
@@ -0,0 +1,24 @@
+package leti_sisdis_6.hapappointmentrecords.service.event;
+
+import java.time.LocalDateTime;
+import leti_sisdis_6.hapappointmentrecords.model.AppointmentStatus;
+import leti_sisdis_6.hapappointmentrecords.model.ConsultationType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AppointmentCanceledEvent {
+
+    private String appointmentId;
+    private String patientId;
+    private String physicianId;
+    private LocalDateTime dateTime;
+    private ConsultationType consultationType;
+    private AppointmentStatus previousStatus;
+    private AppointmentStatus newStatus;
+    private String reason;
+    private LocalDateTime occurredAt;
+}
